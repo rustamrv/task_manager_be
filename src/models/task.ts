47b8e53225dde7a1664413cc5adc41e5691d268c
rustamrv <rystamrv@gmail.com>
2025-01-14 21:37:52 +0200

    add positions

--- src/models/task.ts
+++ src/models/task.ts
@@ -6,12 +6,14 @@ export interface ITask extends Document {
   dueDate: Date;
   status: "to-do" | "in-progress" | "done";
   assignee: mongoose.Types.ObjectId;
+  position: number;
 }
 
 const TaskSchema: Schema = new Schema({
   title: { type: String, required: true },
   description: { type: String },
   dueDate: { type: Date },
+  position: { type: Number, default: 0 },
   status: { type: String, enum: ["to-do", "in-progress", "done"], default: "to-do" },
   assignee: { type: Schema.Types.ObjectId, ref: "User" }, 
 });
--- src/routes/task.ts
+++ src/routes/task.ts
@@ -23,12 +23,18 @@ router.post("/",
   const { title, description, dueDate, status, assignee } = req.body;
 
   try {    
+    const maxPosition = await Task.find({ status })
+    .sort({ position: -1 })
+    .limit(1)
+    .then((tasks) => (tasks.length > 0 ? tasks[0].position : 0));
+
     const newTask = new Task({
       title,
       description,
       dueDate,
       status,
       assignee, 
+      position: maxPosition + 1,
     });
     await newTask.save();
 
@@ -40,7 +46,9 @@ router.post("/",
  
 router.get("/", authenticateToken, async (req: any, res: Response) => {
   try {
-    const tasks = await Task.find({}).populate("assignee", "username email");
+    const tasks = await Task.find({})
+     .sort({ status: 1, position: 1 })
+     .populate("assignee", "username email");
 
      const groupedTasks = {
       "to-do": [],
@@ -56,6 +64,7 @@ router.get("/", authenticateToken, async (req: any, res: Response) => {
         status: task.status,
         dueDate: task.dueDate,
         assignee: task.assignee,
+        position: task.position,
       });
     });
  
@@ -67,15 +76,45 @@ router.get("/", authenticateToken, async (req: any, res: Response) => {
  
 router.put("/:id", authenticateToken, async (req: any, res: Response): Promise<any> => {
   const { id } = req.params;
-  const { title, description, dueDate, status, assignee } = req.body;
+  const { title, description, dueDate, status, assignee, position } = req.body;
 
   try {
-    const updateFields: any = {};
-     if (title) updateFields.title = title; 
-     if (description) updateFields.description = description; 
-     if (dueDate) updateFields.dueDate = dueDate; 
-     if (status) updateFields.status = status; 
-     if (assignee) updateFields.assignee = assignee; 
+      const existingTask = await Task.findById(id);
+      if (!existingTask) {
+        return res.status(404).json({ error: "Task not found" });
+      }
+      const updateFields: any = {};
+      if (title) updateFields.title = title; 
+      if (description) updateFields.description = description; 
+      if (dueDate) updateFields.dueDate = dueDate; 
+      if (status) updateFields.status = status; 
+      if (assignee) updateFields.assignee = assignee;   
+
+      if (status && position !== undefined) {   
+        if (position === 0) {
+          // Переместить текущую задачу на позицию           
+          await Task.updateMany(
+            {
+              status: status,
+              position: { $gte: 0 },
+              _id: { $ne: id },
+            },
+            { $inc: { position: 1 } }
+          );   
+          updateFields.position = 0;
+        } else {
+
+          await Task.updateMany(
+            {
+              status: status,
+              position: { $gte: position },
+              _id: { $ne: id },
+            },
+            { $inc: { position: 1 } }
+          );
+          updateFields.position = position;
+        }
+      }
 
      const updatedTask = await Task.findByIdAndUpdate( 
         id, updateFields, 
@@ -97,6 +136,11 @@ router.delete("/:id", authenticateToken, async (req: any, res: Response) : Promi
   try {
     const deletedTask = await Task.findByIdAndDelete(id);
     if (!deletedTask) return res.status(404).json({ error: "Task not found" });
+
+    await Task.updateMany(
+      { status: deletedTask.status, position: { $gt: deletedTask.position } },
+      { $inc: { position: -1 } }
+    );
 
     res.status(200).json({ message: "Task deleted" });
   } catch (error) {
